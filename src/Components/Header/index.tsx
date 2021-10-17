cc8f011765e436aaa0463044df959b5b29674a36
DausterBarbosa <dausterbarbosa12@gmail.com>
2021-10-17 10:36:19 -0300

    Header done

--- src/Components/Header/index.tsx
+++ src/Components/Header/index.tsx
@@ -1,20 +1,22 @@
-import {FaShoppingCart} from "react-icons/fa";
+import {BsCart2} from "react-icons/bs";
 
-import Logo from "../../Assets/Images/logo.png";
+import Logo from "../../Assets/Images/logo.svg";
 
 import "./styles.css";
 
 function Header(){
     return (
         <header id="header">
-            <img src={Logo} alt="Logo" />
+            <div className="container">
+                <img src={Logo} alt="Logo" />
 
-            <div className="cartButton">
-                <div className="sidePanel">
-                    <strong>Meu carrinho</strong>
-                    <p>1 item</p>
+                <div className="cartButton">
+                    <div className="sidePanel">
+                        <strong>Meu carrinho</strong>
+                        <p>0 item(s)</p>
+                    </div>
+                    <BsCart2 size={25}/>
                 </div>
-                <FaShoppingCart size={30} color="#ffcd33"/>
             </div>
         </header>
     );
