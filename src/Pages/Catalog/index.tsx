31f59deda0c4df586b014d5632e2a930573f2ee5
DausterBarbosa <dausterbarbosa12@gmail.com>
2021-10-19 20:11:58 -0300

    catalog configured

--- src/Pages/Catalog/index.tsx
+++ src/Pages/Catalog/index.tsx
@@ -1,145 +1,47 @@
+import React, {useEffect, useState} from "react";
+
+import Api from "../../services/api";
+
 import {MdOutlineAddShoppingCart} from "react-icons/md";
 
 import "./styles.css";
 
+interface Product {
+    id: number,
+    description: string,
+    image: string,
+    price: number,
+    formatted_price: string,
+}
+
 function Catalog(){
+    const [products, setProducts] = useState<Product[]>([]);
+
+    useEffect(() => {
+        Api.get<Product[]>("/products").then(({data}) => {
+            setProducts(data);
+        });
+    }, [])
+
     return (
         <div id="catalog">
             <ul>
-                <li>
-                    <img src="https://gloimg.gbtcdn.com/soa/gb/item/6575801921400676352/16172/goods_img_big-v17/29f138422045.jpg" alt="" />
-                    <div className="description">
-                        <strong>Smartwatch com tela sensíve ao toque de 1,71 Polegadas.</strong>
-                        <p>R$ 144,00</p>
-                    </div>
-                    <div className="addbutton">
-                        <div className="quantinfo">
-                            <MdOutlineAddShoppingCart color="#FFF" size={25}/>
-                            <p>1</p>
+                {products.map(product => (
+                    <li key={product.id}>
+                        <img src={product.image} alt={product.description} />
+                        <div className="description">
+                            <strong>{product.description}</strong>
+                            <p>{product.formatted_price}</p>
                         </div>
-                        <strong>ADICIONAR AO CARRINHO</strong>
-                    </div>
-                </li>
-
-                <li>
-                    <img src="https://gloimg.gbtcdn.com/soa/gb/item/6575801921400676352/16172/goods_img_big-v17/29f138422045.jpg" alt="" />
-                    <div className="description">
-                        <strong>Smartwatch com tela sensíve ao toque de 1,71 Polegadas.</strong>
-                        <p>R$ 144,00</p>
-                    </div>
-                    <div className="addbutton">
-                        <div className="quantinfo">
-                            <MdOutlineAddShoppingCart color="#FFF" size={25}/>
-                            <p>1</p>
+                        <div className="addbutton">
+                            <div className="quantinfo">
+                                <MdOutlineAddShoppingCart color="#FFF" size={25}/>
+                                <p>1</p>
+                            </div>
+                            <strong>ADICIONAR AO CARRINHO</strong>
                         </div>
-                        <strong>ADICIONAR AO CARRINHO</strong>
-                    </div>
-                </li>
-
-                <li>
-                    <img src="https://gloimg.gbtcdn.com/soa/gb/item/6575801921400676352/16172/goods_img_big-v17/29f138422045.jpg" alt="" />
-                    <div className="description">
-                        <strong>Smartwatch com tela sensíve ao toque de 1,71 Polegadas.</strong>
-                        <p>R$ 144,00</p>
-                    </div>
-                    <div className="addbutton">
-                        <div className="quantinfo">
-                            <MdOutlineAddShoppingCart color="#FFF" size={25}/>
-                            <p>1</p>
-                        </div>
-                        <strong>ADICIONAR AO CARRINHO</strong>
-                    </div>
-                </li>
-
-                <li>
-                    <img src="https://gloimg.gbtcdn.com/soa/gb/item/6575801921400676352/16172/goods_img_big-v17/29f138422045.jpg" alt="" />
-                    <div className="description">
-                        <strong>Smartwatch com tela sensíve ao toque de 1,71 Polegadas.</strong>
-                        <p>R$ 144,00</p>
-                    </div>
-                    <div className="addbutton">
-                        <div className="quantinfo">
-                            <MdOutlineAddShoppingCart color="#FFF" size={25}/>
-                            <p>1</p>
-                        </div>
-                        <strong>ADICIONAR AO CARRINHO</strong>
-                    </div>
-                </li>
-
-                <li>
-                    <img src="https://gloimg.gbtcdn.com/soa/gb/item/6575801921400676352/16172/goods_img_big-v17/29f138422045.jpg" alt="" />
-                    <div className="description">
-                        <strong>Smartwatch com tela sensíve ao toque de 1,71 Polegadas.</strong>
-                        <p>R$ 144,00</p>
-                    </div>
-                    <div className="addbutton">
-                        <div className="quantinfo">
-                            <MdOutlineAddShoppingCart color="#FFF" size={25}/>
-                            <p>1</p>
-                        </div>
-                        <strong>ADICIONAR AO CARRINHO</strong>
-                    </div>
-                </li>
-
-                <li>
-                    <img src="https://gloimg.gbtcdn.com/soa/gb/item/6575801921400676352/16172/goods_img_big-v17/29f138422045.jpg" alt="" />
-                    <div className="description">
-                        <strong>Smartwatch com tela sensíve ao toque de 1,71 Polegadas.</strong>
-                        <p>R$ 144,00</p>
-                    </div>
-                    <div className="addbutton">
-                        <div className="quantinfo">
-                            <MdOutlineAddShoppingCart color="#FFF" size={25}/>
-                            <p>1</p>
-                        </div>
-                        <strong>ADICIONAR AO CARRINHO</strong>
-                    </div>
-                </li>
-
-                <li>
-                    <img src="https://gloimg.gbtcdn.com/soa/gb/item/6575801921400676352/16172/goods_img_big-v17/29f138422045.jpg" alt="" />
-                    <div className="description">
-                        <strong>Smartwatch com tela sensíve ao toque de 1,71 Polegadas.</strong>
-                        <p>R$ 144,00</p>
-                    </div>
-                    <div className="addbutton">
-                        <div className="quantinfo">
-                            <MdOutlineAddShoppingCart color="#FFF" size={25}/>
-                            <p>1</p>
-                        </div>
-                        <strong>ADICIONAR AO CARRINHO</strong>
-                    </div>
-                </li>
-
-                <li>
-                    <img src="https://gloimg.gbtcdn.com/soa/gb/item/6575801921400676352/16172/goods_img_big-v17/29f138422045.jpg" alt="" />
-                    <div className="description">
-                        <strong>Smartwatch com tela sensíve ao toque de 1,71 Polegadas.</strong>
-                        <p>R$ 144,00</p>
-                    </div>
-                    <div className="addbutton">
-                        <div className="quantinfo">
-                            <MdOutlineAddShoppingCart color="#FFF" size={25}/>
-                            <p>1</p>
-                        </div>
-                        <strong>ADICIONAR AO CARRINHO</strong>
-                    </div>
-                </li>
-
-                <li>
-                    <img src="https://gloimg.gbtcdn.com/soa/gb/item/6575801921400676352/16172/goods_img_big-v17/29f138422045.jpg" alt="" />
-                    <div className="description">
-                        <strong>Smartwatch com tela sensíve ao toque de 1,71 Polegadas.</strong>
-                        <p>R$ 144,00</p>
-                    </div>
-                    <div className="addbutton">
-                        <div className="quantinfo">
-                            <MdOutlineAddShoppingCart color="#FFF" size={25}/>
-                            <p>1</p>
-                        </div>
-                        <strong>ADICIONAR AO CARRINHO</strong>
-                    </div>
-                </li>
+                    </li>
+                ))}
             </ul>
         </div>
     );
